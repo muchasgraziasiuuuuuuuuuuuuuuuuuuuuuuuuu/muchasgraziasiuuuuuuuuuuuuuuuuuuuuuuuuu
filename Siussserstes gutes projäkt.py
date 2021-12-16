5d70949b2ff6688aa457e0feddd5cf6564f86585
muchasgraziasiuuuuuuuuuuuuuuuuuuuuuuuuu <96242088+muchasgraziasiuuuuuuuuuuuuuuuuuuuuuuuuu@users.noreply.github.com>
2021-12-16 14:13:36 +0100

    Update Siussserstes gutes projäkt.py

---
+++ Siussserstes gutes projäkt.py
@@ -0,0 +1,29 @@
+#muchasgrazias siuuuuuu
+#Dieses Programm prüft ob du weisst wer der Bundespresident der Schweiz ist :).
+
+
+
+i=3
+
+while i > 0:
+ print(i)
+ i = i- 1
+ if i == 0:
+     break
+    
+ x = "antwort"
+x = (input("Bundespresident der Schweiz: "))
+
+if x == "Guy Parmelin":
+     print("Korrekt")
+
+else:
+         print("Falsch")
+         
+         
+         
+  End
+  
+  #a wonderful a wonderfull
+
+ 
